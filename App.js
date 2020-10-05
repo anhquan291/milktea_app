d7e8a6048c6f4155339f171e348d47a081608753
anhquan291 <anhquan291@gmail.com>
2020-10-05 15:07:50 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View} from 'react-native';
+// import {View} from 'react-native';
 //Redux
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
--- src/screens/intro/IntroScreen.js
+++ src/screens/intro/IntroScreen.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-import {useSelector} from 'react-redux';
 
 const IntroScreen = () => {
-  const user = useSelector((state) => state.user.user);
   return (
     <View style={styles.container}>
       <Text>Intro SCREEN</Text>
